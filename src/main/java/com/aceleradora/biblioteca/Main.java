2a3f713938918b1e38e2a6a2ba3111bb1529cb58
BrayanArgradem <brayancosta299@gmail.com>
2020-06-05 17:36:47 -0300

    coloca mensagem de boas-vindas

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -1,8 +1,9 @@
 package com.aceleradora.biblioteca;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-    }
-}
+                System.out.println("Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre");
+            }
+        }
