1b8bd272cf059ae9ef7f5810729ef18f06f4b207
Elliens <helenakray43@gmail.com>
2020-06-10 12:57:46 -0300

    Projeto Biblioteca

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-                System.out.println("Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre");
-            }
-        }
+        String[] Titulos = new String[2];
+        String[] Autorias = new String[2];
+        Titulos[0] = "Título : Program development in Java";
+        Titulos[1] = "Título : Use a Cabeça: Padrões de projeto";
+        Autorias[0] = "Autoria: Barbara Liskov";
+        Autorias[1] = "Autoria: Elisabeth Freeman, Kathy Sierra";
+
+        System.out.println("Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre");
+        System.out.println("Os livros disponíveis são: ");
+        System.out.println("Livro 1: ");
+        System.out.println(Titulos[0]);
+        System.out.println(Autorias[0]);
+        System.out.println("Livro 2: ");
+        System.out.println(Titulos[1]);
+        System.out.println(Autorias[1]);
+
+
+    }
+}
